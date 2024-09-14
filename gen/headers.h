019cbdf555e91aed0291e185b0c020491ee9f044
sonoro1234 <sonoroster@gmail.com>
2024-09-14 17:57:33 +0200

    APIENTRY defined as empty string

--- gen/headers.h
+++ gen/headers.h
@@ -3,9 +3,8 @@
 // Callback function pointer typedefs need to be marked as stdcall under Windows,
 // even if glfw.lua was built on another system, so override this macro
 // and scan for it in cdef.py
-#define APIENTRY GL_APIENTRY
-
+//#define APIENTRY GL_APIENTRY
+#define APIENTRY
 #include <GL/gl.h>
 #include <GL/glu.h>
-//#include <GLFW/glfw3.h>
 #include "glext.h"
